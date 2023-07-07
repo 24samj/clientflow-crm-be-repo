5f74d837089a2f46ea625e75d2543137c0c7d099
pankaj sondhiya <pankajsondhiya780@gamil.com>
2023-07-07 22:14:19 +0530

    add backend

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    DB_NAME: "crm_db",
-    DB_URL:  process.env.MONGODB_URI || "mongodb://localhost/crm_db"
-}
+  DB_NAME: "crm_db",
+  DB_URL:
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.USERNAME_1}:${process.env.PASSWORD}@cluster0.1gsmies.mongodb.net/crm_db`,
+};
