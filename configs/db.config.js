c9928d173dc876cd306151cad8667883f41475c8
Sumit Chakraborty <csumit961@gmail.com>
2023-11-18 14:00:36 +0530

    final-build

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  DB_NAME: "crm_db",
-  DB_URL:
-    process.env.MONGODB_URI ||
-    `mongodb+srv://${process.env.USERNAME_1}:${process.env.PASSWORD}@cluster0.1gsmies.mongodb.net/crm_db`,
+    DB_NAME: "crm_db",
+    DB_URL:
+        process.env.MONGODB_URI ||
+        `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.mob0anj.mongodb.net/crm_db`,
 };
