9dc044e2182432bb31cf3b87e2010c1143ed9fad
Sumit Chakraborty <csumit961@gmail.com>
2023-11-26 19:57:38 +0530

    final-build

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,12 +1,28 @@
-const ticketController = require('../controllers/ticket.controller');
+const ticketController = require("../controllers/ticket.controller");
 const { authJwt, verifyTicketRequestBody } = require("../middlewares");
 
 module.exports = function (app) {
-
-    app.post("/crm/api/v1/tickets/", [authJwt.verifyToken, verifyTicketRequestBody.validateTicketRequestBody], ticketController.createTicket);
-    app.put("/crm/api/v1/tickets/:id", [authJwt.verifyToken, verifyTicketRequestBody.validateTicketStatus], ticketController.updateTicket);
-    app.get("/crm/api/v1/tickets", [authJwt.verifyToken], ticketController.getAllTickets);
-    app.get("/crm/api/v1/tickets/:id", [authJwt.verifyToken], ticketController.getOneTicket);
-
-
-}
+    app.post(
+        "/crm/api/v1/tickets",
+        [
+            authJwt.verifyToken,
+            verifyTicketRequestBody.validateTicketRequestBody,
+        ],
+        ticketController.createTicket
+    );
+    app.put(
+        "/crm/api/v1/tickets/:id",
+        [authJwt.verifyToken, verifyTicketRequestBody.validateTicketStatus],
+        ticketController.updateTicket
+    );
+    app.get(
+        "/crm/api/v1/tickets",
+        [authJwt.verifyToken],
+        ticketController.getAllTickets
+    );
+    app.get(
+        "/crm/api/v1/tickets/:id",
+        [authJwt.verifyToken],
+        ticketController.getOneTicket
+    );
+};
